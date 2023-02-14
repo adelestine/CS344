5ab63b079e360d9d688f2b55539680a5ac1f0d01
adelestine <delestic@oregonstate.edu>
2023-02-14 15:41:38 -0800

    testGitPush

--- Lab3/main.c
+++ Lab3/main.c
@@ -1,10 +1,16 @@
 #include "header.h"
 
-
+/*
+ *  This function gets user input and prints it to the screen
+ *  @return 0
+ *  
+*/
 int getUserInput(){
     printf(":");
-    
-
+    char input[100];
+    fgets(input, 100, stdin);
+    printf("%s", input);
+    return 0;
 }
 
 
