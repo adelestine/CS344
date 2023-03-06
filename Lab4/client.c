027486fd22dc52448da574c46a7e432d3b63a6e1
adelestine <delestic@oregonstate.edu>
2023-03-06 06:37:28 -0800

    inital Committtt

---
+++ Lab4/client.c
@@ -0,0 +1,78 @@
+Skip to content
+cs344
+@cs344/8_3_client.c
+8_3_client.c
+A C repl by cs344
+Open on Replit
+0
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>  // ssize_t
+#include <sys/socket.h> // send(),recv()
+#include <netdb.h>      // gethostbyname()
+
+/**
+* Client code
+* 1. Create a socket and connect to the server specified in the command arugments.
+* 2. Prompt the user for input and send that input as a message to the server.
+* 3. Print the message received from the server and exit the program.
+*/
+
+// Error function used for reporting issues
+void error(const char *msg) { 
+  perror(msg); 
+  exit(0); 
+} 
+
+// Set up the address struct
+void setupAddressStruct(struct sockaddr_in* address, 
+                        int portNumber, 
+                        char* hostname){
+ 
+  // Clear out the address struct
+  memset((char*) address, '\0', sizeof(*address)); 
+
+  // The address should be network capable
+  address->sin_family = AF_INET;
+  // Store the port number
+  address->sin_port = htons(portNumber);
+
+  // Get the DNS entry for this host name
+  struct hostent* hostInfo = gethostbyname(hostname); 
+  if (hostInfo == NULL) { 
+    fprintf(stderr, "CLIENT: ERROR, no such host\n"); 
+    exit(0); 
+  }
+  // Copy the first IP address from the DNS entry to sin_addr.s_addr
+  memcpy((char*) &address->sin_addr.s_addr, 
+        hostInfo->h_addr_list[0],
+        hostInfo->h_length);
+}
+
+int main(int argc, char *argv[]) {
+  int socketFD, portNumber, charsWritten, charsRead;
+  struct sockaddr_in serverAddress;
+  char buffer[256];
+  // Check usage & args
+  if (argc < 3) { 
+    fprintf(stderr,"USAGE: %s hostname port\n", argv[0]); 
+    exit(0); 
+  } 
+
+  // Create a socket
+  socketFD = socket(AF_INET, SOCK_STREAM, 0); 
+  if (socketFD < 0){
+    error("CLIENT: ERROR opening socket");
+  }
+
+   // Set up the server address struct
+  setupAddressStruct(&serverAddress, atoi(argv[2]), argv[1]);
+
+  // Connect to server
+  if (connect(socketFD, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0){
+    error("CLIENT: ERROR connecting");
+Files
+
+8_3_client.c - Replit
