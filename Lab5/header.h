645a11acecb3c5b94e78634b8c5a9ab1aa8c1c8f
adelestine <delestic@oregonstate.edu>
2023-03-15 08:46:06 -0700

    working w/o threading

---
+++ Lab5/header.h
@@ -0,0 +1,25 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <pthread.h>
+
+
+#define LINE_SEP '\n'
+
+struct Buffer
+{
+    char *data;
+    int size;
+};
+
+
+
+
+
+
+#endif
